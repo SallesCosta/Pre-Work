e99776e616755b68d385902b92c52ed922bdd03c
SallesCosta <newcapital.in@gmail.com>
2021-08-17 15:54:09 +0200

    novas function

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 import './style.css'
 import './main2.js'
 
-// quando usamos o getElementsByTagName ou Id.. ou Name, temos uma HTMLCollection
-// HTML Collection => ela é 'viva'... conforme eu adiciono novas tags <div>.. o console muda a quantidade
-// se ele muda a quantidade ele é exatamente o contrário do princípio da IMUTABILIDADE... nao recomendado hj em dia
-// evita efeitos colaterais
+//// quando usamos o getElementsByTagName ou Id.. ou Name, temos uma HTMLCollection
+//// HTML Collection => ela é 'viva'... conforme eu adiciono novas tags <div>.. o console muda a quantidade
+//// se ele muda a quantidade ele é exatamente o contrário do princípio da IMUTABILIDADE... nao recomendado hj em dia
+//// evita efeitos colaterais
 
 // const a = document.getElementsByTagName('div')
 // console.log(a.length)  //ver no console  HTMLCollection
@@ -14,7 +14,7 @@ import './main2.js'
 // console.log('a:', a.length)
 
 
-// quando usamos o querySelectorAll, temos uma NodeList
+// //quando usamos o querySelectorAll, temos uma NodeList
 // const a = document.querySelectorAll('div')    //temos uma div
 // console.log('a: ', a.length)
 
@@ -24,9 +24,30 @@ import './main2.js'
 // const b = document.querySelectorAll('div')    //b é a soma de todas as divs
 // console.log('b: ', b.length)
 
-//no console vai aparecer
+//// no console vai aparecer
 // a: 1
 // b: 2
 
 const app = document.querySelector('[data-js="app"]')
-console.log(app)
+const link = document.querySelector('[data-js="link"]')
+const button = document.querySelector('[data-js="button"]')
+
+link.addEventListener('click', (event)=>{
+  event.preventDefault()
+  alert(link)
+  console.log('clicou S this', event.currentTarget)
+}, false) //este false que é o terceiro argumento pode ser omitido pois hoje ele é falso por padrão. O evento vai se "propagar de dentro pra fora "
+// é uma lazy fuction... ela só funfa quando o comando dela é executado.. neste caso.. quando ela é clicada
+
+// essa é a mesma função da de cima.. poré com o this. Para que o 'this' funcione.. nao pode ser arrowFunction
+link.addEventListener('click', function(event){
+  event.preventDefault()
+  event.stopPropagation()////caso eu queira clicar no button mas que a função do link nao execute
+console.log('clicou com this', this)
+}, false)
+
+button.addEventListener('click',()=>{
+  alert(button)
+},false)
+
+////se quiser inverter a ordem de propagação dos eventos.. é só colocar true no link
