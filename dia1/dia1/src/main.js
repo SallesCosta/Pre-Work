8ef4a0df23fbff2c59941a4cef8451b174470a46
SallesCosta <newcapital.in@gmail.com>
2021-08-17 18:58:31 +0200

    Fix Exercício 4 e +

--- dia1/dia1/src/main.js
+++ dia1/dia1/src/main.js
@@ -4,9 +4,26 @@ const app = document.querySelector('[data-js="app"]').innerHTML = `
 <h1>B. Academy</h1>
 <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
 `
+const link = document.querySelector('[data-js="link"]')
 
-const showHide = document.querySelector('[data-js="link"]')
+link.addEventListener('click', ()=>{
+  const a = document.querySelector('[data-js="app"]')
+  if(a.classList.contains('hide')){
+    link.innerHTML = `<h3>Esconder</h3>`
+    a.classList.remove('hide')
+  }else{
+    link.innerHTML = `<h3>Mostrar</h3>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <a href="https://github.com/SallesCosta" target="_blank" class="button">
+    <span><i class="fa fa-github" aria-hidden="true"></i> Github</span></a>
+      <p>uma estrelinha pra um dev que está começando???</p>
+      <p>valeu!!!</p>`
+    a.classList.add('hide')
+  }
+}, false)
 
-showHide.addEventListener('click', ()=>{
-  document.querySelector('[data-js="app"]').style.setProperty("display", "none")
-})
