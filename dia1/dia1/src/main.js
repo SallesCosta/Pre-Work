6da8a739d35f0032cd98e5ac1a5419ffaebae530
SallesCosta <newcapital.in@gmail.com>
2021-08-17 17:02:34 +0200

    Fix Exercício 4

--- dia1/dia1/src/main.js
+++ dia1/dia1/src/main.js
@@ -1,6 +1,12 @@
 import './style.css'
 
-document.querySelector('[data-js="app"]').innerHTML = `
-  <h1>B. Academy</h1>
-  <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
+const app = document.querySelector('[data-js="app"]').innerHTML = `
+<h1>B. Academy</h1>
+<p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
 `
+
+const showHide = document.querySelector('[data-js="link"]')
+
+showHide.addEventListener('click', ()=>{
+  document.querySelector('[data-js="app"]').style.setProperty("display", "none")
+})
