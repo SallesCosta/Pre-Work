30168dc88257bea504caaecd25e6cbcff3ff04c0
SallesCosta <newcapital.in@gmail.com>
2021-08-18 11:18:41 +0200

    Add dia 2

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-import './style.css'
-import './main2.js'
+// import './style.css'
+// import './main2.js'
 
 //// quando usamos o getElementsByTagName ou Id.. ou Name, temos uma HTMLCollection
 //// HTML Collection => ela é 'viva'... conforme eu adiciono novas tags <div>.. o console muda a quantidade
@@ -28,26 +28,53 @@ import './main2.js'
 // a: 1
 // b: 2
 
-const app = document.querySelector('[data-js="app"]')
-const link = document.querySelector('[data-js="link"]')
-const button = document.querySelector('[data-js="button"]')
+// const app = document.querySelector('[data-js="app"]')
+// const link = document.querySelector('[data-js="link"]')
+// const button = document.querySelector('[data-js="button"]')
 
-link.addEventListener('click', (event)=>{
-  event.preventDefault()
-  alert(link)
-  console.log('clicou S this', event.currentTarget)
-}, false) //este false que é o terceiro argumento pode ser omitido pois hoje ele é falso por padrão. O evento vai se "propagar de dentro pra fora "
-// é uma lazy fuction... ela só funfa quando o comando dela é executado.. neste caso.. quando ela é clicada
+// link.addEventListener('click', (event)=>{
+//   event.preventDefault()
+//   alert(link)
+//   console.log('clicou S this', event.currentTarget)
+// }, false) //este false que é o terceiro argumento pode ser omitido pois hoje ele é falso por padrão. O evento vai se "propagar de dentro pra fora "
+// // é uma lazy fuction... ela só funfa quando o comando dela é executado.. neste caso.. quando ela é clicada
 
-// essa é a mesma função da de cima.. poré com o this. Para que o 'this' funcione.. nao pode ser arrowFunction
-link.addEventListener('click', function(event){
-  event.preventDefault()
-  event.stopPropagation()////caso eu queira clicar no button mas que a função do link nao execute
-console.log('clicou com this', this)
-}, false)
+// // essa é a mesma função da de cima.. poré com o this. Para que o 'this' funcione.. nao pode ser arrowFunction
+// link.addEventListener('click', function(event){
+//   event.preventDefault()
+//   event.stopPropagation()////caso eu queira clicar no button mas que a função do link nao execute
+// console.log('clicou com this', this)
+// }, false)
 
-button.addEventListener('click',()=>{
-  alert(button)
-},false)
+// button.addEventListener('click',()=>{
+//   alert(button)
+// },false)
 
 ////se quiser inverter a ordem de propagação dos eventos.. é só colocar true no link
+
+//----------------------------------------------------------------------------------------------
+
+////Anotacoes do dia 2
+
+import './style.css'
+
+let name = "Eduardo"
+
+function render({ title, description }) {
+
+  document.querySelector('#app').innerHTML = `
+  <h1>Hello ${title}</h1>
+  ${description ? `<p>${description}</p>` : ""}
+  `
+}
+const link = document.querySelector('[data-js="link"]')
+link.addEventListener('click', (e) => {
+  e.preventDefault()
+  render({ title: 'Eduardo' })
+})
+
+render({ title: 'Vite', description: 'vite é veloz' })
+
+// Higher Order Functions => é uma função extraordinaria
+// pode retornar uma function por argumento e pode fornecer outra função
+
