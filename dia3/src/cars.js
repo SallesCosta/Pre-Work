3167f46f583cd1a2c3676da631736e4b5946048c
SallesCosta <newcapital.in@gmail.com>
2021-08-20 17:32:57 +0200

    Dia 4 - Exercício 3

--- dia3/src/cars.js
+++ dia3/src/cars.js
@@ -13,24 +13,60 @@ const getFormElement = (event) => (elementName) => {
   return event.target.elements[elementName]
 }
 
+const elementsTypes = {
+  image: createImage,
+  text: createText,
+  color: createColor
+}
+
+function createImage(value) {
+  const td = document.createElement('td')
+  const img = document.createElement('img')
+  img.src = value
+  img.width = 100
+  td.appendChild(img)
+  return td
+}
+
+function createText(value) {
+  const td = document.createElement('td')
+  td.textContent = value
+  return td
+}
+
+function createColor(value) {
+  const td = document.createElement('td')
+  const div = document.createElement('div')
+  div.style.width = '100px'
+  div.style.height = '100px'
+  div.style.background = value
+  td.appendChild(div)
+  return td
+
+}
+
+
 form.addEventListener('submit', (e) => {
   e.preventDefault()
   const getElement = getFormElement(e)
-  const image = getElement('image')
-  const brandModel = getElement('brand-model')
-  const year = getElement('year')
-  const plate = getElement('plate')
-  const color = getElement('color')
 
-  const elements = ['image', 'brand-model', 'year', 'plate', 'color']
+  const elements = [
+    { type: 'image', value: getElement('image').value },
+    { type: 'text', value: getElement('brand-model').value },
+    { type: 'text', value: getElement('year').value },
+    { type: 'text', value: getElement('plate').value },
+    { type: 'color', value: getElement('color').value }
+  ]
 
   const tr = document.createElement('tr')
-  elements.forEach(element =>{
-    const td = document.createElement('td')
-    td.textContent = element.value
-    tr.appendChild(td )
+  elements.forEach(element => {
+    const td = elementsTypes[element.type](element.value)
+    tr.appendChild(td)
   })
   table.appendChild(tr)
+
+  e.target.reset()
+  image.focus()
 })
 
 
