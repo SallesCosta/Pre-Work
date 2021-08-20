5af5f21b785bb52f43a929bcc178d217a5c028b5
SallesCosta <newcapital.in@gmail.com>
2021-08-20 15:43:03 +0200

    Dia 4 - Exercício 2

--- dia3/src/cars.js
+++ dia3/src/cars.js
@@ -1,35 +1,35 @@
-// function c(x) {
-//   console.log(x)
+function c(x) {
+  console.log(x)
+}
+
+function $(x) {
+  return document.querySelector(`[data-js='${x}']`)
+}
+
+// const form  = document.querySelector('[data-js="cars-form"]')
+// const table  = document.querySelector('[data-js="table"]')
+// const getFormElement = (event) => (elementName) => {
+//   return event.target.elements[elementName]
 // }
 
-// function $(x) {
-//   document.querySelector('[data-js="{x}"]')
-// }
 
-// // const form  = document.querySelector('[data-js="cars-form"]')
-// // const table  = document.querySelector('[data-js="table"]')
-// // const getFormElement = (event) => (elementName) => {
-// //   return event.target.elements[elementName]
-// // }
-
-
-// // form.addEventListener('submit', (e)=>{
-// //   e.preventDefault()
-// //   const getElement = getFormElement(e)
-// //   const image = getElement('image')
-// //   const brandModel = getElement('brand-model')
-// //   const year = getElement('image')
-// //   const plate = getElement('image')
-// //   const color = getElement('image')
-// // })
+// form.addEventListener('submit', (e)=>{
+//   e.preventDefault()
+//   const getElement = getFormElement(e)
+//   const image = getElement('image')
+//   const brandModel = getElement('brand-model')
+//   const year = getElement('image')
+//   const plate = getElement('image')
+//   const color = getElement('image')
+// })
 
 
 
-// fetch('http://example.com/movies.json')
-//   .then(response => response.json())
-//   .then(data => console.log(data));
+fetch('http://example.com/movies.json')
+  .then(response => response.json())
+  .then(data => console.log(data));
 
-// fetch()
+fetch()
 
 
 
--- dia3/src/form.js
+++ dia3/src/form.js
@@ -34,78 +34,32 @@ const form = $('form')
 const select = document.createElement('select')
 const colors = ['#6B7280', '#EF4444', '#F59E0B', '#10B981', '#3B82F6', '#6366F1', '#8B5CF6', '#EC4899']
 
-// const colorsContainer = $('div')
-// colorsContainer.style.display = 'flex'
+const colorsContainer = document.createElement('div')
+colorsContainer.style.display = 'flex'
 
 colors.forEach(color => {
   const option = document.createElement('option')
   option.value=color
   option.textConten = color
+  option.style.width = '200px'
   select.appendChild(option)
 })
 
-// select.addEventListener('change', (e)=>{
-//   colorsContainer.innerHTML = ''
-//   Array.from(e.target.selectedOptions).map(option =>{
-//     const div = document.createElement('div')
-//     div.style.width = '100px'
-//     div.style.height = '100px'
-//     div.style.backgroundColor = option.value
-//     colorsContainer.appendChild(div)
-//   })
-// })
+ select.addEventListener('change', (e)=>{
+  colorsContainer.innerHTML = ''
+  Array.from(e.target.selectedOptions).map(option =>{
+    const div = document.createElement('div')
+    div.style.width = '100px'
+    div.style.height = '100px'
+    div.style.backgroundColor = option.value
+    colorsContainer.appendChild(div)
+  })
+})
 
 select.setAttribute('multiple', '')
 form.appendChild(select)
+document.body.appendChild(colorsContainer)
 c(select)
-
-
-
-// Exercício 3
-
-// console.log (inputName)
-// console.log(e.target.value)
-//   const txt =  e.target.value = e.target.value.toLowerCase()   //lega
-//   const txt2 = txt[0].toUpperCase()
-//   console.log(txt, txt2)
-// let toLowerCase = e.target.value.toLowerCase()
-
-// const abc = "eduardo salles da costa"
-
-// function capitalizeFirst(str) {
-// const subst = str.toLowerCase().replace(/(?:^|\s)\S/g, function(a) {
-//   return a.toUpperCase();
-// })
-// return subst
-// }
-
-// console.log(capitalizeFirst(abc))
-
-// console.log([...e.target.selectedOptions].map(el => el.value))
-
-
-// const app = document.querySelector('[data-js="app"]')
-
-// const select = document.querySelector('[data-js="select"]')
-// select.addEventListener('change', (e) => {
-//   [...e.target.selectedOptions].map(el => {
-//     const div = document.createElement('div')
-//     const id = el.id
-//     div.innerHTML = `<div style="width: 100px;height: 100px; background-color: ${id};"></div>`
-//     app.appendChild(div)
-//   })
-// })
-
-
-
-
-
-
-
-
-
-
-
 
 
 
