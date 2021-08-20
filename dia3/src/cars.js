bb610db8e731e19d79aef3393bdd5ff60014013b
SallesCosta <newcapital.in@gmail.com>
2021-08-20 10:31:45 +0200

    ---

--- dia3/src/cars.js
+++ dia3/src/cars.js
@@ -1,35 +1,35 @@
-function c(x) {
-  console.log(x)
-}
-
-function $(x) {
-  document.querySelector('[data-js="{x}"]')
-}
-
-// const form  = document.querySelector('[data-js="cars-form"]')
-// const table  = document.querySelector('[data-js="table"]')
-// const getFormElement = (event) => (elementName) => {
-//   return event.target.elements[elementName]
+// function c(x) {
+//   console.log(x)
 // }
 
+// function $(x) {
+//   document.querySelector('[data-js="{x}"]')
+// }
 
-// form.addEventListener('submit', (e)=>{
-//   e.preventDefault()
-//   const getElement = getFormElement(e)
-//   const image = getElement('image')
-//   const brandModel = getElement('brand-model')
-//   const year = getElement('image')
-//   const plate = getElement('image')
-//   const color = getElement('image')
-// })
-
+// // const form  = document.querySelector('[data-js="cars-form"]')
+// // const table  = document.querySelector('[data-js="table"]')
+// // const getFormElement = (event) => (elementName) => {
+// //   return event.target.elements[elementName]
+// // }
 
 
-fetch('http://example.com/movies.json')
-  .then(response => response.json())
-  .then(data => console.log(data));
+// // form.addEventListener('submit', (e)=>{
+// //   e.preventDefault()
+// //   const getElement = getFormElement(e)
+// //   const image = getElement('image')
+// //   const brandModel = getElement('brand-model')
+// //   const year = getElement('image')
+// //   const plate = getElement('image')
+// //   const color = getElement('image')
+// // })
 
-fetch()
+
+
+// fetch('http://example.com/movies.json')
+//   .then(response => response.json())
+//   .then(data => console.log(data));
+
+// fetch()
 
 
 
--- dia3/src/form.js
+++ dia3/src/form.js
@@ -1,8 +1,23 @@
-// const inputName = document.querySelector('[data-js="username"]')
+function c(x) {
+  console.log(x)
+}
+
+function $(x) {
+  return document.querySelector(`[data-js='${x}']`)
+}
+
+function $$(x) {
+  return document.querySelectorAll(`[data-js='${x}']`)
+}
+
+
+
+// // Exercício 1
+// const inputName = $('username')
 // const dontChang = ['da', 'das', 'de', 'do', 'dos']
 // inputName.addEventListener('input', (e) => {
 //   const valueAsArray = e.target.value.split(' ')
-//   console.log(valueAsArray)
+//   c(valueAsArray)
 //   e.target.value = valueAsArray.map((word) => {
 //     return dontChang.includes(word.toLowerCase())
 //     ? word.toLowerCase()
@@ -10,21 +25,24 @@
 //   }).join(' ')
 // })
 // function fixCase(word) {
-//     return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+//   return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
 // }
 
+// Exercício 2
 // const form = document.querySelector('[data-js="form"]')
-// const select = document.querySelector('[data-js="select"]')
-// const colors = []
-// const colorsContainer = document.querySelector('div')
+const form = $('form')
+const select = document.createElement('select')
+const colors = ['#6B7280', '#EF4444', '#F59E0B', '#10B981', '#3B82F6', '#6366F1', '#8B5CF6', '#EC4899']
+
+// const colorsContainer = $('div')
 // colorsContainer.style.display = 'flex'
 
-// colors.forEach(color => {
-//   const option = document.createElement('option')
-//   option.value=color
-//   option.textConten = color
-//   select.appendChild(option)
-// })
+colors.forEach(color => {
+  const option = document.createElement('option')
+  option.value=color
+  option.textConten = color
+  select.appendChild(option)
+})
 
 // select.addEventListener('change', (e)=>{
 //   colorsContainer.innerHTML = ''
@@ -37,12 +55,13 @@
 //   })
 // })
 
-// select.setAttribute('multiple', '')
-// form.appendChild(select)
+select.setAttribute('multiple', '')
+form.appendChild(select)
+c(select)
 
 
 
-
+// Exercício 3
 
 // console.log (inputName)
 // console.log(e.target.value)
@@ -76,4 +95,17 @@
 //     app.appendChild(div)
 //   })
 // })
+
+
+
+
+
+
+
+
+
+
+
+
+
 
